2053268d9d157e1de4f1bb3fd41c8020fe8ded65
DoHyeon Jeon <whdies816@gmail.com>
2020-01-23 17:35:35 +0900

    Todo Header

---
+++ Task2/todolist/src/components/TodoHeader/TodoHeader.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './TodoHeader.css';
+
+const TodoHeader: React.SFC = () => {
+  return (
+    <div className="header">
+      <div className="headerText">
+        <div>What's you will</div>
+        <div>gonna Do??</div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoHeader;
