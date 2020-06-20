231571642942e862cf0e68c24d682c382ba08ee8
DoHyeon Jeon <whdies816@gmail.com>
2020-06-20 14:58:10 +0900

    useScroll

--- Task7/react-hook/src/App.js
+++ Task7/react-hook/src/App.js
@@ -1,35 +1,30 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
-const useNetwork = (onChange) => {
-  const [status, setStatus] = useState(navigator.onLine);
-  const handleChange = () => {
-    if (typeof onChange === 'function') {
-      onChange(navigator.onLine);
-    }
-    setStatus(navigator.onLine);
+const useScroll = () => {
+  const [state, setState] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const onScroll = () => {
+    setState({ x: window.scrollX, y: window.scrollY });
   };
 
   useEffect(() => {
-    window.addEventListener('online', handleChange);
-    window.addEventListener('offline', handleChange);
-    return () => {
-      window.removeEventListener('online', handleChange);
-      window.removeEventListener('offine', handleChange);
-    };
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
   }, []);
-
-  return status;
+  return state;
 };
 
 const App = () => {
-  const handleNetwordChange = (online) => {
-    console.log(online ? 'We Just want online' : 'we are offine');
-  };
-  const online = useNetwork(handleNetwordChange);
+  const { y } = useScroll();
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="App" style={{ height: '150vh' }}>
+      <h1 style={{ position: 'fixed', color: y > 100 ? 'red' : 'blue' }}>
+        Hello
+      </h1>
     </div>
   );
 };
